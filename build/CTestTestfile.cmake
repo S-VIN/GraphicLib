784f54fb25b2560713addf273b2f82dcfe9e7590
S-VIN <7530023@gmail.com>
2020-09-20 10:42:08 +0300

    add cmake files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/stepan/Yandex.Disk/Documents/homeProjects/GraphicLib
+# Build directory: /home/stepan/Yandex.Disk/Documents/homeProjects/GraphicLib/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
