1c349a78c56fd723b9b536b7d338f083a88467a7
S-VIN <7530023@gmail.com>
2020-10-03 12:54:09 +0300

    add non working class

--- main.cpp
+++ main.cpp
@@ -3,11 +3,52 @@ using namespace sf;
 
 void print(Printer &);
 
+class VectorPrint {
+private:
+  Printer * printer;
+  struct Point{
+    int x;
+    int y;
+  };
+  struct Figure{
+    Point point;
+    int a;
+  };
+  std::vector<Figure> mas;
+  void printSquare(Figure figure){
+    for(int i = figure.point.x; i < figure.point.x + figure.a; i++){
+      printer->setPixel(i, figure.point.y);
+    }
+    for(int i = figure.point.x; i < figure.point.x + figure.a; i++){
+      printer->setPixel(i, figure.point.y + figure.a);
+    }
+
+    for(int i = figure.point.y; i < figure.point.y + figure.a; i++){
+      printer->setPixel(figure.point.x, i);
+    }
+
+    for(int i = figure.point.y; i < figure.point.y + figure.a; i++){
+      printer->setPixel(figure.point.x + figure.a, i);
+    }
+
+  }
+
+public:
+  VectorPrint(Printer * printer){
+    VectorPrint:: printer = printer;
+  }
+  void square(Point point, int a){
+    mas.push_back(Figure{point, a});
+    printSquare(mas[0]);
+  }
+};
+
+
+
 int main() {
   Printer printer;
   while (printer.isOpen()) {
     print(printer);
-    printer.setPixel(50, 50, Color::Red);
     sf::Event event;
     while (printer.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
@@ -19,10 +60,13 @@ int main() {
 }
 
 void print(Printer &printer) {
-  // printer.clearBuffer();
+  /*printer.clearScreen();
   printer.setPixel(0, 0, Color::Red);
   printer.setPixel(50, 50, Color::Red);
-  printer.setPixel(50, 50, Color::Red);
   printer.setPixel(99, 99, Color::Red);
-  printer.show();
+  printer.show();*/
+
+  VectorPrint vectorPrint(&printer);
+  vectorPrint.square({50,50}, 10);
+
 }
